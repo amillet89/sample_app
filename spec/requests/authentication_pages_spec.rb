341f97725c1910a126d14142787f50c0433fbcf5
Alex Millet <alex.millet89@gmail.com>
2012-06-25 23:14:47 +0200

    Midway in finishing user edit, update, index, destroy

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -24,29 +24,18 @@ describe "Authentication" do
       describe "after visiting another page" do
             before { click_link "Home" }
             it { should_not have_selector('div.alert.alert-error') }
-      end
-          
-      describe "with valid information" do
-          let(:user) { FactoryGirl.create(:user) }
-          before do
-            fill_in "Email",    with: user.email
-            fill_in "Password", with: user.password
-            click_button "Sign in"
-            
-          describe "followed by signout" do
-            before { click_link "Sign out" }
-            it { should have_link('Sign in') }
-          end
-      end
+      end  
+  
+  
+   describe "with valid information" do
+        let(:user) { FactoryGirl.create(:user) }
+        before { sign_in user }
 
-          it { should have_selector('title', text: user.name) }
-          it { should have_link('Profile', href: user_path(user)) }
-          it { should have_link('Sign out', href: signout_path) }
-          it { should_not have_link('Sign in', href: signin_path) }
-  end
-  
-  
-  
-  
+        it { should have_selector('title', text: user.name) }
+        it { should have_link('Profile', href: user_path(user)) }
+        it {should have_link('Settings', href: edit_user_path(user))}
+        it { should have_link('Sign out', href: signout_path) }
+        it { should_not have_link('Sign in', href: signin_path) }
+    end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,6 +3,24 @@ require 'spec_helper'
 describe "User pages" do
 
   subject { page }
+  
+  describe "edit" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { visit edit_user_path(user) }
+
+      describe "page" do
+        it { should have_selector('h1',    text: "Update your profile") }
+        it { should have_selector('title', text: "Edit user") }
+        it { should have_link('change', href: 'http://gravatar.com/emails') }
+      end
+
+      describe "with invalid information" do
+        before { click_button "Save changes" }
+
+        it { should have_content('error') }
+      end
+  end
+  
 
   describe "signup page" do
     before { visit signup_path }
