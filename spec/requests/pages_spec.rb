9e429bdde374fe80b43d29b346d3919093140a62
Alex Millet <alex.millet89@gmail.com>
2012-06-27 01:34:20 +0200

    Add user following

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -10,7 +10,36 @@ describe "Static pages" do
     it { should have_selector('h1',    text: 'Sample App') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }
-  end
+  
+    describe "for signed-in users" do
+          let(:user) { FactoryGirl.create(:user) }
+          before do
+            FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+            FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+            sign_in user
+            visit root_path
+          end
+
+          it "should render the user's feed" do
+            user.feed.each do |item|
+              page.should have_selector("li##{item.id}", text: item.content)
+            end
+          end # should render
+     
+     
+     describe "follower/following counts" do
+             let(:other_user) { FactoryGirl.create(:user) }
+             before do
+               other_user.follow!(user)
+               visit root_path
+             end
+
+             it { should have_link("0 following", href: following_user_path(user)) }
+             it { should have_link("1 followers", href: followers_user_path(user)) }
+     end    # follower/following counts
+   end # for signed-in users
+  
+  end # home page
 
   describe "Help page" do
     before { visit help_path }
@@ -32,21 +61,5 @@ describe "Static pages" do
     it { should have_selector('h1',    text: 'Contact') }
     it { should have_selector('title', text: full_title('Contact')) }
   end
-  
-  describe "for signed-in users" do
-        let(:user) { FactoryGirl.create(:user) }
-        before do
-          FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-          FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
-          sign_in user
-          visit root_path
-        end
-
-        it "should render the user's feed" do
-          user.feed.each do |item|
-            page.should have_selector("li##{item.id}", text: item.content)
-          end
-        end # should render
-   end # for signed-in users
   
 end # Static pages
