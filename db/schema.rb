22ebf91cc2c6404cb63d5a5d0dd6c4e480906d6c
amillet89 <alex.millet89@gmail.com>
2013-01-11 17:20:33 -0500

    Testing dev environment git push on windows

--- db/schema.rb
+++ db/schema.rb
@@ -36,13 +36,14 @@ ActiveRecord::Schema.define(:version => 20120626204619) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin"
+    t.boolean  "admin",           :default => false
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
